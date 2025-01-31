2df37b03373b8ca604f4518cf2778d3bfbe391ed
mithu31 <smithushan2@gmail.com>
2025-01-31 19:42:57 +0530

    Update SplashCursor.jsx

--- src/components/SplashCursor.jsx
+++ src/components/SplashCursor.jsx
@@ -14,7 +14,7 @@ function SplashCursor({
   SPLAT_RADIUS = 0.2,
   SPLAT_FORCE = 6000,
   SHADING = true,
-  COLOR_UPDATE_SPEED = 20,
+  COLOR_UPDATE_SPEED = 10,
   BACK_COLOR = { r: 0.5, g: 0, b: 0 },
   TRANSPARENT = true,
 }) {
