7d135f484d7833bb938b5bad3abc1c29a575f43f
Mithushan Sachchithananthan <118989562+mithushan03@users.noreply.github.com>
2026-01-07 10:45:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Certifications  from "./components/Certifications";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Certifications from "./components/Certifications";
 import Project from "./components/Project";
-
-
-
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="pt-20">
       <Header />
-      <Home />
-      <About />
-      <Experience />
-      <Certifications  />
-      <Project />
-      <Contact />
+      <main>
+        <Home />
+        <About />
+        <Experience />
+        <Certifications />
+        <Project />
+        <Contact />
+      </main>
       <Footer />
-      
-      
     </div>
   );
 };
+
 export default App;
